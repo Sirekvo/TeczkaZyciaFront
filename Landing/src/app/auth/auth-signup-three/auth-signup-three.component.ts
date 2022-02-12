0fa8db1db355886513fc324527caa6f8126f5587
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-02-12 22:09:01 +0100

    skonczone osoby kontaktowe, zaczete alergie

--- Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
+++ Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
@@ -9,8 +9,10 @@ import { Component, OnInit } from '@angular/core';
 /**
  * Auth Signup-three Component
  */
+
 export class AuthSignupThreeComponent implements OnInit {
 
+  i=0;
   constructor() { }
 
   ngOnInit(): void {
@@ -18,16 +20,61 @@ export class AuthSignupThreeComponent implements OnInit {
   // tslint:disable-next-line:typedef
   contact(){
     document.getElementById('first').style.display = 'none';
-    document.getElementById('myDropdown').style.display = 'block';
+    document.getElementById('hidden_1').style.display = 'block';
+  }
+  alergy(){
+    document.getElementById('first').style.display = 'none';
+    document.getElementById('hidden_2').style.display = 'block';
   }
   back_to_start(){
     document.getElementById('first').style.display = 'block';
-    document.getElementById('myDropdown').style.display = 'none';
+    document.getElementById('hidden_1').style.display = 'none';
+    document.getElementById('hidden_2').style.display = 'none';
   }
   back_to_start_complete(){
     document.getElementById('first').style.display = 'block';
-    document.getElementById('myDropdown').style.display = 'none';
+    document.getElementById('hidden_1').style.display = 'none';
     document.getElementById('first_contact').style.display = 'none';
     document.getElementById('complete_contact').style.display = 'block';
+  }
+  back_to_start_complete_2(){
+    document.getElementById('first').style.display = 'block';
+    document.getElementById('hidden_2').style.display = 'none';
+    document.getElementById('alergy').style.display = 'none';
+    document.getElementById('complete_alergy').style.display = 'block';
+  }
+
+  fill_table() {
+    this.i++;
+    if(this.i==1){
+      document.getElementById('1').innerHTML = "1";
+      if ((<HTMLInputElement>document.getElementById('family')).checked) {
+        document.getElementById('2').innerHTML = (<HTMLInputElement>document.getElementById('family')).value;
+      }
+      else if((<HTMLInputElement>document.getElementById('friend')).checked) {
+        document.getElementById('2').innerHTML = (<HTMLInputElement>document.getElementById('friend')).value;
+      }
+      document.getElementById('3').innerHTML = (<HTMLInputElement>document.getElementById('phone')).value;
+    }
+    else if(this.i==2){
+      document.getElementById('4').innerHTML = "2";
+      if ((<HTMLInputElement>document.getElementById('family')).checked) {
+        document.getElementById('5').innerHTML = (<HTMLInputElement>document.getElementById('family')).value;
+      }
+      else if((<HTMLInputElement>document.getElementById('friend')).checked) {
+        document.getElementById('5').innerHTML = (<HTMLInputElement>document.getElementById('friend')).value;
+      }
+      document.getElementById('6').innerHTML = (<HTMLInputElement>document.getElementById('phone')).value;
+    }
+    else if(this.i==3){
+      document.getElementById('7').innerHTML = "3";
+      if ((<HTMLInputElement>document.getElementById('family')).checked) {
+        document.getElementById('8').innerHTML = (<HTMLInputElement>document.getElementById('family')).value;
+      }
+      else if((<HTMLInputElement>document.getElementById('friend')).checked) {
+        document.getElementById('8').innerHTML = (<HTMLInputElement>document.getElementById('friend')).value;
+      }
+      document.getElementById('9').innerHTML = (<HTMLInputElement>document.getElementById('phone')).value;
+    }
   }
 }
