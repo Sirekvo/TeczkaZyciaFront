5f494ab79d25c37dfe2ba61b920a683cc462c378
Piotr Świerczyński <piotrswierczynski50@gmail.com>
2022-02-12 01:08:27 +0100

    Wstepny starter v2

--- Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
+++ Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
@@ -15,5 +15,19 @@ export class AuthSignupThreeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  // tslint:disable-next-line:typedef
+  contact(){
+    document.getElementById('first').style.display = 'none';
+    document.getElementById('myDropdown').style.display = 'block';
+  }
+  back_to_start(){
+    document.getElementById('first').style.display = 'block';
+    document.getElementById('myDropdown').style.display = 'none';
+  }
+  back_to_start_complete(){
+    document.getElementById('first').style.display = 'block';
+    document.getElementById('myDropdown').style.display = 'none';
+    document.getElementById('first_contact').style.display = 'none';
+    document.getElementById('complete_contact').style.display = 'block';
+  }
 }
