58b1d5f928eb4a70f854b026694e085514159ea4
sh0wd0wn <96078689+sh0wd0wn@users.noreply.github.com>
2022-02-14 17:05:31 +0100

    lista_kontaktow_v2
    próba usuwanie, zmiana działania listy kontaktów

--- Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
+++ Landing/src/app/auth/auth-signup-three/auth-signup-three.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
+import { ContactClass } from './contactClass';
 
 @Component({
   selector: 'app-auth-signup-three',
@@ -11,6 +13,9 @@ import { Component, OnInit } from '@angular/core';
  */
 
 export class AuthSignupThreeComponent implements OnInit {
+  type: number;
+  telephone: string;
+  contactList: Array<ContactClass> = [];
 
   i=0;
   constructor() { }
@@ -21,6 +26,7 @@ export class AuthSignupThreeComponent implements OnInit {
   contact(){
     document.getElementById('first').style.display = 'none';
     document.getElementById('hidden_1').style.display = 'block';
+    // console.log("we cos wypisz");
   }
   alergy(){
     document.getElementById('first').style.display = 'none';
@@ -100,4 +106,96 @@ export class AuthSignupThreeComponent implements OnInit {
       document.getElementById('9').innerHTML = (<HTMLInputElement>document.getElementById('phone')).value;
     }
   }
+
+  // delete_row_contact(){
+  //   console.log("Funcka sie uruchomila");
+  //   // this.contactList.splice(index_del, 1);
+  //   // this.newCreateTable();
+  //   // console.log("pelna lista obiektow po usunieciu");
+  //   // for(let j=0;j<this.contactList.length;j++){
+  //   //   console.log("typ elementu "+j+" : " + this.contactList[j].type);
+  //   //   console.log("telefon elementu "+j+" : " + this.contactList[j].telephone);
+  //   // }
+  // }
+  
+  delete_row_contact(){
+    console.log("Funkcja sie uruchomila");
+    // this.contactList.splice(index_del, 1);
+    // this.newCreateTable();
+    // console.log("pelna lista obiektow po usunieciu");
+    // for(let j=0;j<this.contactList.length;j++){
+    //   console.log("typ elementu "+j+" : " + this.contactList[j].type);
+    //   console.log("telefon elementu "+j+" : " + this.contactList[j].telephone);
+    // }
+  }
+
+  newCreateTable(){
+    let button_contact:HTMLButtonElement=<HTMLButtonElement>document.createElement("button");
+    button_contact.innerText = "Usun";
+    //button_contact.onclick= () => this.delete_row_contact();
+
+    //button_contact.onclick =delete_row_contact;
+    var mytable = `<table class='table mb-0 table-center' >
+    <thead>
+      <tr>
+        <th scope=col>#</th>
+        <th scope='col'>Kto</th>
+        <th scope='col'>Numer telefonu</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>`;
+    for(let n=0;n<this.contactList.length;n++){
+      var typeInner:string;
+      if(this.contactList[n].type==1){
+        typeInner="Rodzina";
+      }
+      else if(this.contactList[n].type==2){
+        typeInner="Znajomy";
+      }
+        mytable+="<th scope='row'>"+(n+1)+"</th><td>"+typeInner+"</td><td>"+this.contactList[n].telephone+"</td></tr>"
+        // document.scripts[document.scripts.length -1 ].appendChild(button_contact.onclick= delete_row_contact).outerHTML + " </td></tr>";
+   
+          //mytable+="<th scope='row'>"+(n+1)+"</th><td>"+typeInner+"</td><td>"+this.contactList[n].telephone+"</td>" + "<td>" + button_contact.outerHTML + "</td></tr>";
+          
+        //  var button_contact= document.querySelector("#usun_test");
+    
+    
+    // button_contact.addEventListener("click", function(){
+      // console.log("Dziala");
+    // });
+    }
+    mytable+="</tbody></table>";
+    document.getElementById("contactTable").innerHTML = mytable;
+  }
+
+  onContactCreate(){
+    let customObj = new ContactClass();
+    if ((<HTMLInputElement>document.getElementById('family')).checked) {
+      customObj.type=1;
+    }
+    else if((<HTMLInputElement>document.getElementById('friend')).checked) {
+      customObj.type=2;
+    }
+    // customObj.type=1;
+    customObj.telephone=(<HTMLInputElement>document.getElementById('phone')).value;
+    this.contactList.push(customObj);
+    this.type = 0;
+    this.telephone="";
+    console.log("pelna lista obiektow");
+    for(let j=0;j<this.contactList.length;j++){
+      console.log("typ elementu "+j+" : " + this.contactList[j].type);
+      console.log("telefon elementu "+j+" : " + this.contactList[j].telephone);
+      this.delete_row_contact();
+    }
+    // console.log("typ: " + customObj.type );
+    // console.log("numer: " + customObj.telephone );
+    // console.log("cos sie wyswielta");
+  }
 }
+
+
+function delete_row(): (this: HTMLButtonElement, ev: MouseEvent) => any {
+  throw new Error('dziala.');
+}
+
---
+++ Landing/src/app/auth/auth-signup-three/contactClass.ts
@@ -0,0 +1,4 @@
+export class ContactClass{
+    type: number;
+    telephone: string;
+}
